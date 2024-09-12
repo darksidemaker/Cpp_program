4f7f92cbd870cebafd143e3af888626312aec75c
darksidemaker <113779888+darksidemaker@users.noreply.github.com>
2024-09-12 15:17:33 +0000

    ass

--- tollboth.cpp
+++ tollboth.cpp
@@ -5,27 +5,35 @@ class TollBoth
 private:
     unsigned int numberCars;
     double moneyCollected;
+    unsigned int payingCars;
+    unsigned int nonPayingCars;
 
 public:
     TollBoth()
     {
         numberCars = 0;
         moneyCollected = 0;
+        payingCars = 0;
+        nonPayingCars = 0;
     }
 
     void display()
     {
         cout << "Number of Cars passed by  toll booth: " << numberCars << endl;
         cout << "Toll Both money collected: $" << moneyCollected << endl;
+        cout << "Number of paying cars: " << payingCars << endl;
+        cout << "Number of non paying cars: " << nonPayingCars << endl;
     }
     void carPay()
     {
         numberCars++;
         moneyCollected += 0.50;
+        payingCars++;
     }
     void noCarpay()
     {
         numberCars++;
+        nonPayingCars++;
     }
 };
 
@@ -45,24 +53,6 @@ int main()
     day1.noCarpay();
 
     day1.display();
-
-    TollBoth day2;
-    day2.carPay();
-    day2.carPay();
-    day2.carPay();
-
-    day2.noCarpay();
-    day2.noCarpay();
-    day2.carPay();
-    day2.carPay();
-    day2.carPay();
-    day2.carPay();
-    day2.noCarpay();
-    day2.noCarpay();
-    day2.noCarpay();
-    day2.noCarpay();
-
-    day2.display();
 
     return 0;
 }
